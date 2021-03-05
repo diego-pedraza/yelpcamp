15f209796fc6c76a2039d82cb8039ee497676040
Diego Pedraza <diegopedraza@Diegos-MacBook-Pro.local>
2021-03-05 18:22:22 +0100

    Reestructuring and Flash implementation

---
+++ routes/campgrounds.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const router = express.Router();
+const { campgroundSchema } = require("../schemas");
+
+const Campground = require("../models/campground");
+
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+
+const validateCampground = (req, res, next) => {
+	const { error } = campgroundSchema.validate(req.body);
+
+	if (error) {
+		const msg = error.details.map((el) => el.message).join(",");
+		throw new ExpressError(msg, 400);
+	} else {
+		next();
+	}
+};
+
+router.get(
+	"/",
+	catchAsync(async (req, res) => {
+		const campgrounds = await Campground.find({});
+		res.render("campgrounds/index", { campgrounds });
+	})
+);
+
+router.get("/new", (req, res) => {
+	res.render("campgrounds/new");
+});
+
+router.post(
+	"/",
+	validateCampground,
+	catchAsync(async (req, res, next) => {
+		// if (!req.body.campground) {
+		// 	throw new ExpressError("Invalid campground data", 400);
+		// }
+		const campground = new Campground(req.body.campground);
+		await campground.save();
+		req.flash("success", "Successfully made a new Campground!");
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
+
+router.get(
+	"/:id",
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		const campground = await Campground.findById(id).populate("reviews");
+		if (!campground) {
+			req.flash("error", "Campground doesn't exist!");
+			return res.redirect("/campgrounds");
+		}
+		res.render("campgrounds/show", { campground });
+	})
+);
+
+router.get(
+	"/:id/edit",
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		const campground = await Campground.findById(id);
+		if (!campground) {
+			req.flash("error", "Campground doesn't exist!");
+			return res.redirect("/campgrounds");
+		}
+		res.render("campgrounds/edit", { campground });
+	})
+);
+
+router.put(
+	"/:id",
+	validateCampground,
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		const campground = await Campground.findByIdAndUpdate(id, {
+			...req.body.campground,
+		});
+		req.flash("success", "Successfully modified the campground!");
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
+
+router.delete(
+	"/:id",
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		await Campground.findByIdAndDelete(id);
+		req.flash("success", "Successfully deleted the campground");
+		res.redirect(`/campgrounds`);
+	})
+);
+
+module.exports = router;
